e86971f401de510db52dade35571daaf53f5b435
darkgl0w <31093081+darkgl0w@users.noreply.github.com>
2021-12-04 23:08:25 +0100

    test: rewrite tests

--- test/headers.test.js
+++ test/headers.test.js
@@ -1,269 +1,302 @@
 'use strict'
 
-const http = require('http')
-const test = require('tap').test
+const { test } = require('tap')
+const Fastify = require('fastify')
 const plugin = require('../plugin')
-const fastify = require('fastify')
 
-test('decorators get added', (t) => {
+test('decorators get added', async (t) => {
   t.plan(1)
-  const instance = fastify()
-  instance.register(plugin)
-  instance.get('/', (req, reply) => {
+
+  const fastify = Fastify()
+  await fastify.register(plugin)
+
+  t.teardown(fastify.close.bind(fastify))
+
+  fastify.get('/', (req, reply) => {
     t.ok(reply.etag)
     reply.send()
   })
-  instance.listen(0, (err) => {
-    if (err) t.threw(err)
-    instance.server.unref()
-    const portNum = instance.server.address().port
-    const address = `http://127.0.0.1:${portNum}`
-    http.get(address, (res) => {}).on('error', (err) => t.threw(err))
+
+  await fastify.ready()
+
+  await fastify.inject({
+    method: 'GET',
+    path: '/'
   })
 })
 
-test('decorators add headers', (t) => {
+test('decorators add headers', async (t) => {
   t.plan(2)
+
   const tag = '123456'
-  const instance = fastify()
-  instance.register(plugin)
-  instance.get('/', (req, reply) => {
+
+  const fastify = Fastify()
+  await fastify.register(plugin)
+
+  t.teardown(fastify.close.bind(fastify))
+
+  fastify.get('/', (req, reply) => {
     reply
       .etag(tag)
       .send()
   })
-  instance.listen(0, (err) => {
-    if (err) t.threw(err)
-    instance.server.unref()
-    const portNum = instance.server.address().port
-    const address = `http://127.0.0.1:${portNum}`
-    http.get(address, (res) => {
-      t.ok(res.headers.etag)
-      t.equal(res.headers.etag, tag)
-    }).on('error', (err) => t.threw(err))
+
+  await fastify.ready()
+
+  const response = await fastify.inject({
+    method: 'GET',
+    path: '/'
   })
+  t.ok(response.headers.etag)
+  t.equal(response.headers.etag, tag)
 })
 
-test('sets etag header for falsy argument', (t) => {
+test('sets etag header for falsy argument', async (t) => {
   t.plan(1)
-  const instance = fastify()
-  instance.register(plugin)
-  instance.get('/', (req, reply) => {
+
+  const fastify = Fastify()
+  await fastify.register(plugin)
+
+  t.teardown(fastify.close.bind(fastify))
+
+  fastify.get('/', (req, reply) => {
     reply
       .etag()
       .send()
   })
-  instance.listen(0, (err) => {
-    if (err) t.threw(err)
-    instance.server.unref()
-    const portNum = instance.server.address().port
-    const address = `http://127.0.0.1:${portNum}`
-    http.get(address, (res) => {
-      t.ok(res.headers.etag)
-    }).on('error', (err) => t.threw(err))
+
+  await fastify.ready()
+
+  const response = await fastify.inject({
+    method: 'GET',
+    path: '/'
   })
+  t.ok(response.headers.etag)
 })
 
-test('sets no-cache header', (t) => {
+test('sets no-cache header', async (t) => {
   t.plan(2)
-  const instance = fastify()
-  instance.register(plugin, { privacy: plugin.privacy.NOCACHE })
-  instance.get('/', (req, reply) => {
+
+  const fastify = Fastify()
+  await fastify.register(plugin, { privacy: plugin.privacy.NOCACHE })
+
+  t.teardown(fastify.close.bind(fastify))
+
+  fastify.get('/', (req, reply) => {
     reply.send({ hello: 'world' })
   })
-  instance.listen(0, (err) => {
-    if (err) t.threw(err)
-    instance.server.unref()
-    const portNum = instance.server.address().port
-    const address = `http://127.0.0.1:${portNum}`
 
-    http.get(address, (res) => {
-      t.ok(res.headers['cache-control'])
-      t.equal(res.headers['cache-control'], 'no-cache')
-    }).on('error', (err) => t.threw(err))
+  await fastify.ready()
+
+  const response = await fastify.inject({
+    method: 'GET',
+    path: '/'
   })
+  t.ok(response.headers['cache-control'])
+  t.equal(response.headers['cache-control'], 'no-cache')
 })
 
-test('sets private with max-age header', (t) => {
+test('sets private with max-age header', async (t) => {
   t.plan(2)
-  const instance = fastify()
+
   const opts = {
     privacy: plugin.privacy.PRIVATE,
     expiresIn: 300
   }
-  instance.register(plugin, opts)
-  instance.get('/', (req, reply) => {
+
+  const fastify = Fastify()
+  await fastify.register(plugin, opts)
+
+  t.teardown(fastify.close.bind(fastify))
+
+  fastify.get('/', (req, reply) => {
     reply.send({ hello: 'world' })
   })
-  instance.listen(0, (err) => {
-    if (err) t.threw(err)
-    instance.server.unref()
-    const portNum = instance.server.address().port
-    const address = `http://127.0.0.1:${portNum}`
 
-    http.get(address, (res) => {
-      t.ok(res.headers['cache-control'])
-      t.equal(res.headers['cache-control'], 'private, max-age=300')
-    }).on('error', (err) => t.threw(err))
+  await fastify.ready()
+
+  const response = await fastify.inject({
+    method: 'GET',
+    path: '/'
   })
+  t.ok(response.headers['cache-control'])
+  t.equal(response.headers['cache-control'], 'private, max-age=300')
 })
 
-test('sets public with max-age and s-maxage header', (t) => {
+test('sets public with max-age and s-maxage header', async (t) => {
   t.plan(2)
-  const instance = fastify()
+
   const opts = {
     privacy: plugin.privacy.PUBLIC,
     expiresIn: 300,
     serverExpiresIn: 12345
   }
-  instance.register(plugin, opts)
-  instance.get('/', (req, reply) => {
+
+  const fastify = Fastify()
+  await fastify.register(plugin, opts)
+
+  t.teardown(fastify.close.bind(fastify))
+
+  fastify.get('/', (req, reply) => {
     reply.send({ hello: 'world' })
   })
-  instance.listen(0, (err) => {
-    if (err) t.threw(err)
-    instance.server.unref()
-    const portNum = instance.server.address().port
-    const address = `http://127.0.0.1:${portNum}`
 
-    http.get(address, (res) => {
-      t.ok(res.headers['cache-control'])
-      t.equal(res.headers['cache-control'], 'public, max-age=300, s-maxage=12345')
-    }).on('error', (err) => t.threw(err))
+  await fastify.ready()
+
+  const response = await fastify.inject({
+    method: 'GET',
+    path: '/'
   })
+  t.ok(response.headers['cache-control'])
+  t.equal(response.headers['cache-control'], 'public, max-age=300, s-maxage=12345')
 })
 
-test('only sets max-age and ignores s-maxage with private header', (t) => {
+test('only sets max-age and ignores s-maxage with private header', async (t) => {
   t.plan(2)
-  const instance = fastify()
+
   const opts = {
     privacy: plugin.privacy.PRIVATE,
     expiresIn: 300,
     serverExpiresIn: 12345
   }
-  instance.register(plugin, opts)
-  instance.get('/', (req, reply) => {
+
+  const fastify = Fastify()
+  await fastify.register(plugin, opts)
+
+  t.teardown(fastify.close.bind(fastify))
+
+  fastify.get('/', (req, reply) => {
     reply.send({ hello: 'world' })
   })
-  instance.listen(0, (err) => {
-    if (err) t.threw(err)
-    instance.server.unref()
-    const portNum = instance.server.address().port
-    const address = `http://127.0.0.1:${portNum}`
 
-    http.get(address, (res) => {
-      t.ok(res.headers['cache-control'])
-      t.equal(res.headers['cache-control'], 'private, max-age=300')
-    }).on('error', (err) => t.threw(err))
+  await fastify.ready()
+
+  const response = await fastify.inject({
+    method: 'GET',
+    path: '/'
   })
+  t.ok(response.headers['cache-control'])
+  t.equal(response.headers['cache-control'], 'private, max-age=300')
 })
 
-test('s-maxage is optional with public header', (t) => {
+test('s-maxage is optional with public header', async (t) => {
   t.plan(2)
-  const instance = fastify()
+
   const opts = {
     privacy: plugin.privacy.PUBLIC,
     expiresIn: 300
   }
-  instance.register(plugin, opts)
-  instance.get('/', (req, reply) => {
+
+  const fastify = Fastify()
+  await fastify.register(plugin, opts)
+
+  t.teardown(fastify.close.bind(fastify))
+
+  fastify.get('/', (req, reply) => {
     reply.send({ hello: 'world' })
   })
-  instance.listen(0, (err) => {
-    if (err) t.threw(err)
-    instance.server.unref()
-    const portNum = instance.server.address().port
-    const address = `http://127.0.0.1:${portNum}`
 
-    http.get(address, (res) => {
-      t.ok(res.headers['cache-control'])
-      t.equal(res.headers['cache-control'], 'public, max-age=300')
-    }).on('error', (err) => t.threw(err))
+  await fastify.ready()
+
+  const response = await fastify.inject({
+    method: 'GET',
+    path: '/'
   })
+  t.ok(response.headers['cache-control'])
+  t.equal(response.headers['cache-control'], 'public, max-age=300')
 })
 
-test('sets no-store with max-age header', (t) => {
+test('sets no-store with max-age header', async (t) => {
   t.plan(2)
-  const instance = fastify()
-  instance.register(plugin, { privacy: 'no-store', expiresIn: 300 })
-  instance.get('/', (req, reply) => {
+
+  const fastify = Fastify()
+  await fastify.register(plugin, { privacy: 'no-store', expiresIn: 300 })
+
+  t.teardown(fastify.close.bind(fastify))
+
+  fastify.get('/', (req, reply) => {
     reply.send({ hello: 'world' })
   })
-  instance.listen(0, (err) => {
-    if (err) t.threw(err)
-    instance.server.unref()
-    const portNum = instance.server.address().port
-    const address = `http://127.0.0.1:${portNum}`
 
-    http.get(address, (res) => {
-      t.ok(res.headers['cache-control'])
-      t.equal(res.headers['cache-control'], 'no-store, max-age=300')
-    }).on('error', (err) => t.threw(err))
+  await fastify.ready()
+
+  const response = await fastify.inject({
+    method: 'GET',
+    path: '/'
   })
+  t.ok(response.headers['cache-control'])
+  t.equal(response.headers['cache-control'], 'no-store, max-age=300')
 })
 
-test('sets the expires header', (t) => {
+test('sets the expires header', async (t) => {
   t.plan(2)
+
   const now = new Date()
-  const instance = fastify()
-  instance.register(plugin, { privacy: plugin.privacy.NOCACHE })
-  instance.get('/', (req, reply) => {
+
+  const fastify = Fastify()
+  await fastify.register(plugin, { privacy: plugin.privacy.NOCACHE })
+
+  t.teardown(fastify.close.bind(fastify))
+
+  fastify.get('/', (req, reply) => {
     reply
       .expires(now)
       .send({ hello: 'world' })
   })
-  instance.listen(0, (err) => {
-    if (err) t.threw(err)
-    instance.server.unref()
-    const portNum = instance.server.address().port
-    const address = `http://127.0.0.1:${portNum}`
 
-    http.get(address, (res) => {
-      t.ok(res.headers.expires)
-      t.equal(res.headers.expires, now.toUTCString())
-    }).on('error', (err) => t.threw(err))
+  await fastify.ready()
+
+  const response = await fastify.inject({
+    method: 'GET',
+    path: '/'
   })
+  t.ok(response.headers.expires)
+  t.equal(response.headers.expires, now.toUTCString())
 })
 
-test('sets the expires header to a falsy value', (t) => {
+test('sets the expires header to a falsy value', async (t) => {
   t.plan(1)
-  const instance = fastify()
-  instance.register(plugin, { privacy: plugin.privacy.NOCACHE })
-  instance.get('/', (req, reply) => {
+
+  const fastify = Fastify()
+  await fastify.register(plugin, { privacy: plugin.privacy.NOCACHE })
+
+  t.teardown(fastify.close.bind(fastify))
+
+  fastify.get('/', (req, reply) => {
     reply
       .expires()
       .send({ hello: 'world' })
   })
-  instance.listen(0, (err) => {
-    if (err) t.threw(err)
-    instance.server.unref()
-    const portNum = instance.server.address().port
-    const address = `http://127.0.0.1:${portNum}`
 
-    http.get(address, (res) => {
-      t.notOk(res.headers.expires)
-    }).on('error', (err) => t.threw(err))
+  await fastify.ready()
+
+  const response = await fastify.inject({
+    method: 'GET',
+    path: '/'
   })
+  t.notOk(response.headers.expires)
 })
 
-test('sets the expires header to a custom value', (t) => {
+test('sets the expires header to a custom value', async (t) => {
   t.plan(2)
-  const instance = fastify()
-  instance.register(plugin, { privacy: plugin.privacy.NOCACHE })
-  instance.get('/', (req, reply) => {
+
+  const fastify = Fastify()
+  await fastify.register(plugin, { privacy: plugin.privacy.NOCACHE })
+
+  t.teardown(fastify.close.bind(fastify))
+
+  fastify.get('/', (req, reply) => {
     reply
       .expires('foobar')
       .send({ hello: 'world' })
   })
-  instance.listen(0, (err) => {
-    if (err) t.threw(err)
-    instance.server.unref()
-    const portNum = instance.server.address().port
-    const address = `http://127.0.0.1:${portNum}`
 
-    http.get(address, (res) => {
-      t.ok(res.headers.expires)
-      t.equal(res.headers.expires, 'foobar')
-    }).on('error', (err) => t.threw(err))
+  await fastify.ready()
+
+  const response = await fastify.inject({
+    method: 'GET',
+    path: '/'
   })
+  t.ok(response.headers.expires)
+  t.equal(response.headers.expires, 'foobar')
 })
