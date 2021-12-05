1d7b828d265aa233d6b5ffe30693408fe93442ed
darkgl0w <31093081+darkgl0w@users.noreply.github.com>
2021-12-05 19:51:17 +0100

    test: prepare plugin for fastify 4.x (#84)
    * test: replace `preParsing` hooks with `onRequest` hooks
    
    * chore (dev-deps): update package dependencies
    
    * test: rewrite tests
    
    * ci: add Node.js 17
    
    * fix (test): use async hooks
    
    * test: clean up
    
    * test: clean up

--- test/cache.test.js
+++ test/cache.test.js
@@ -1,130 +1,132 @@
 'use strict'
 
-const http = require('http')
-const test = require('tap').test
+const { test } = require('tap')
 const plugin = require('../plugin')
 
-const fastify = require('fastify')
+const Fastify = require('fastify')
 
-test('cache property gets added to instance', (t) => {
+test('cache property gets added to instance', async (t) => {
   t.plan(2)
-  const instance = fastify()
-  instance
-    .register(plugin)
-    .ready(() => {
-      t.ok(instance.cache)
-      t.ok(instance.cache.set)
-    })
+
+  const fastify = Fastify()
+  await fastify.register(plugin)
+  await fastify.ready()
+
+  t.ok(fastify.cache)
+  t.ok(fastify.cache.set)
 })
 
-test('cache is usable', (t) => {
+test('cache is usable', async (t) => {
   t.plan(3)
-  const instance = fastify()
-  instance
-    .register((i, o, n) => {
-      i.addHook('onRequest', function (req, reply, done) {
-        t.notOk(i[Symbol.for('fastify-caching.registered')])
-        done()
-      })
-      n()
-    })
-    .register(plugin)
 
-  instance.addHook('preParsing', function (req, reply, payload, done) {
+  const fastify = Fastify()
+  await fastify.register(async (instance, options) => {
+    instance.addHook('onRequest', async function (req, reply) {
+      t.notOk(instance[Symbol.for('fastify-caching.registered')])
+    })
+  })
+  await fastify.register(plugin)
+
+  fastify.addHook('onRequest', async function (req, reply) {
     t.equal(this[Symbol.for('fastify-caching.registered')], true)
-    done(null, payload)
   })
 
-  instance.get('/one', (req, reply) => {
-    instance.cache.set('one', { one: true }, 100, (err) => {
+  fastify.get('/one', (req, reply) => {
+    fastify.cache.set('one', { one: true }, 100, (err) => {
       if (err) return reply.send(err)
       reply.redirect('/two')
     })
   })
 
-  instance.get('/two', (req, reply) => {
-    instance.cache.get('one', (err, obj) => {
+  fastify.get('/two', (req, reply) => {
+    fastify.cache.get('one', (err, obj) => {
       if (err) t.threw(err)
       t.same(obj.item, { one: true })
       reply.send()
     })
   })
 
-  instance.listen(0, (err) => {
-    if (err) t.threw(err)
-    instance.server.unref()
-    const portNum = instance.server.address().port
-    const address = `http://127.0.0.1:${portNum}/one`
-    http
-      .get(address, (res) => {
-        if (res.statusCode > 300 && res.statusCode < 400 && res.headers.location) {
-          http.get(`http://127.0.0.1:${portNum}${res.headers.location}`, (res) => {}).on('error', t.threw)
-        }
-      })
-      .on('error', t.threw)
+  await fastify.ready()
+
+  const response = await fastify.inject({
+    method: 'GET',
+    path: '/one'
   })
+
+  if (
+    response.statusCode > 300 &&
+    response.statusCode < 400 &&
+    response.headers.location
+  ) {
+    await fastify.inject({
+      method: 'GET',
+      path: response.headers.location
+    })
+  }
 })
 
-test('cache is usable with function as plugin default options input', (t) => {
+test('cache is usable with function as plugin default options input', async (t) => {
   t.plan(3)
-  const instance = fastify()
-  instance
-    .register((i, o, n) => {
-      i.addHook('onRequest', function (req, reply, done) {
-        t.notOk(i[Symbol.for('fastify-caching.registered')])
-        done()
-      })
-      n()
-    })
-    .register(plugin, () => () => { })
 
-  instance.addHook('preParsing', function (req, reply, payload, done) {
+  const fastify = Fastify()
+  await fastify.register(async (instance, options) => {
+    instance.addHook('onRequest', async function (req, reply) {
+      t.notOk(instance[Symbol.for('fastify-caching.registered')])
+    })
+  })
+  await fastify.register(plugin, () => () => { })
+
+  fastify.addHook('onRequest', async function (req, reply) {
     t.equal(this[Symbol.for('fastify-caching.registered')], true)
-    done(null, payload)
   })
 
-  instance.get('/one', (req, reply) => {
-    instance.cache.set('one', { one: true }, 100, (err) => {
+  fastify.get('/one', (req, reply) => {
+    fastify.cache.set('one', { one: true }, 100, (err) => {
       if (err) return reply.send(err)
       reply.redirect('/two')
     })
   })
 
-  instance.get('/two', (req, reply) => {
-    instance.cache.get('one', (err, obj) => {
+  fastify.get('/two', (req, reply) => {
+    fastify.cache.get('one', (err, obj) => {
       if (err) t.threw(err)
       t.same(obj.item, { one: true })
       reply.send()
     })
   })
 
-  instance.listen(0, (err) => {
-    if (err) t.threw(err)
-    instance.server.unref()
-    const portNum = instance.server.address().port
-    const address = `http://127.0.0.1:${portNum}/one`
-    http
-      .get(address, (res) => {
-        if (res.statusCode > 300 && res.statusCode < 400 && res.headers.location) {
-          http.get(`http://127.0.0.1:${portNum}${res.headers.location}`, (res) => { }).on('error', t.threw)
-        }
-      })
-      .on('error', t.threw)
+  await fastify.ready()
+
+  const response = await fastify.inject({
+    method: 'GET',
+    path: '/one'
   })
+
+  if (
+    response.statusCode > 300 &&
+    response.statusCode < 400 &&
+    response.headers.location
+  ) {
+    await fastify.inject({
+      method: 'GET',
+      path: response.headers.location
+    })
+  }
 })
 
-test('getting cache item with error returns error', (t) => {
+test('getting cache item with error returns error', async (t) => {
   t.plan(1)
+
   const mockCache = {
     get: (info, callback) => callback(new Error('cache.get always errors')),
     set: (key, value, ttl, callback) => callback()
   }
 
-  const instance = fastify()
-  instance.register(plugin, { cache: mockCache })
+  const fastify = Fastify()
+  await fastify.register(plugin, { cache: mockCache })
 
-  instance.get('/one', (req, reply) => {
-    instance.cache.set('one', { one: true }, 1000, (err) => {
+  fastify.get('/one', (req, reply) => {
+    fastify.cache.set('one', { one: true }, 1000, (err) => {
       if (err) return reply.send(err)
       return reply
         .etag('123456')
@@ -132,140 +134,121 @@ test('getting cache item with error returns error', (t) => {
     })
   })
 
-  instance.get('/two', (req, reply) => {
-    instance.cache.get('one', (err, obj) => {
+  fastify.get('/two', (req, reply) => {
+    fastify.cache.get('one', (err, obj) => {
       t.notOk(err)
       t.notOk(obj)
     })
   })
 
-  instance.listen(0, (err) => {
-    if (err) t.threw(err)
-    instance.server.unref()
-    const portNum = instance.server.address().port
-    const address = `http://127.0.0.1:${portNum}/one`
+  await fastify.ready()
 
-    http
-      .get(address, (res) => {
-        const opts = {
-          host: '127.0.0.1',
-          port: portNum,
-          path: '/two',
-          headers: {
-            'if-none-match': '123456'
-          }
-        }
-        http.get(opts, (res) => {
-          t.equal(res.statusCode, 500)
-        }).on('error', t.threw)
-      })
-      .on('error', t.threw)
+  await fastify.inject({
+    method: 'GET',
+    path: '/one'
   })
+
+  const response = await fastify.inject({
+    method: 'GET',
+    path: '/two',
+    headers: {
+      'if-none-match': '123456'
+    }
+  })
+  t.equal(response.statusCode, 500)
 })
 
-test('etags get stored in cache', (t) => {
+test('etags get stored in cache', async (t) => {
   t.plan(1)
-  const instance = fastify()
-  instance.register(plugin)
 
-  instance.get('/one', (req, reply) => {
+  const fastify = Fastify()
+  await fastify.register(plugin)
+
+  fastify.get('/one', (req, reply) => {
     reply
       .etag('123456')
       .send({ hello: 'world' })
   })
 
-  instance.listen(0, (err) => {
-    if (err) t.threw(err)
-    instance.server.unref()
-    const portNum = instance.server.address().port
-    const address = `http://127.0.0.1:${portNum}/one`
-    http
-      .get(address, (res) => {
-        const opts = {
-          host: '127.0.0.1',
-          port: portNum,
-          path: '/one',
-          headers: {
-            'if-none-match': '123456'
-          }
-        }
-        http
-          .get(opts, (res) => {
-            t.equal(res.statusCode, 304)
-          })
-          .on('error', t.threw)
-      })
-      .on('error', t.threw)
+  await fastify.ready()
+
+  await fastify.inject({
+    method: 'GET',
+    path: '/one'
   })
+
+  const response = await fastify.inject({
+    method: 'GET',
+    path: '/one',
+    headers: {
+      'if-none-match': '123456'
+    }
+  })
+  t.equal(response.statusCode, 304)
 })
 
 test('etag cache life is customizable', (t) => {
-  t.plan(1)
-  const instance = fastify()
-  instance.register(plugin)
+  t.plan(4)
 
-  instance.get('/one', function (req, reply) {
+  const fastify = Fastify()
+  fastify.register(plugin)
+
+  fastify.get('/one', function (req, reply) {
     reply
+      // We set a cache lifetime of 50 milliseconds
       .etag('123456', 50)
       .send({ hello: 'world' })
   })
 
-  instance.listen(0, (err) => {
-    if (err) t.threw(err)
-    instance.server.unref()
-    const portNum = instance.server.address().port
-    const address = `http://127.0.0.1:${portNum}/one`
-    http
-      .get(address, (res) => {
-        const opts = {
-          host: '127.0.0.1',
-          port: portNum,
+  fastify.ready((err) => {
+    t.error(err)
+
+    fastify.inject({
+      method: 'GET',
+      path: '/one'
+    }, (err, _response) => {
+      t.error(err)
+
+      // We wait 70 milliseconds that the cache expires
+      setTimeout(() => {
+        fastify.inject({
+          method: 'GET',
           path: '/one',
           headers: {
             'if-none-match': '123456'
           }
-        }
-        setTimeout(() => {
-          http
-            .get(opts, (res) => {
-              t.equal(res.statusCode, 200)
-            })
-            .on('error', t.threw)
-        }, 150)
-      })
-      .on('error', t.threw)
+        }, (err, response) => {
+          t.error(err)
+          t.equal(response.statusCode, 200)
+        })
+      }, 70)
+    })
   })
 })
 
-test('returns response payload', (t) => {
+test('returns response payload', async (t) => {
   t.plan(1)
-  const instance = fastify()
-  instance.register(plugin)
 
-  instance.get('/one', (req, reply) => {
+  const fastify = Fastify()
+  await fastify.register(plugin)
+
+  fastify.get('/one', (req, reply) => {
     reply
       .etag('123456', 300)
       .send({ hello: 'world' })
   })
 
-  instance.listen(0, (err) => {
-    if (err) t.threw(err)
-    instance.server.unref()
-    const portNum = instance.server.address().port
-    const opts = {
-      host: '127.0.0.1',
-      port: portNum,
-      path: '/one'
-    }
-    http
-      .get(opts, (res) => {
-        let payload = ''
-        res.on('data', (chunk) => {
-          payload += chunk
-        }).on('end', () => {
-          t.same(JSON.parse(payload), { hello: 'world' })
-        }).on('error', t.threw)
-      })
-      .on('error', t.threw)
+  await fastify.ready()
+
+  await fastify.inject({
+    method: 'GET',
+    path: '/one'
   })
+
+  const response = await fastify.inject({
+    method: 'GET',
+    path: '/one'
+  })
+
+  t.same(JSON.parse(response.payload), { hello: 'world' })
 })
