cf1330f1fa261bea63d0bfec3ee35250042b4039
Frazer Smith <frazer.dev@icloud.com>
2024-12-08 10:44:16 +0000

    build(deps-dev): replace standard with neostandard

--- Readme.md
+++ Readme.md
@@ -2,7 +2,7 @@
 
 [![CI](https://github.com/fastify/fastify-caching/actions/workflows/ci.yml/badge.svg?branch=master)](https://github.com/fastify/fastify-caching/actions/workflows/ci.yml)
 [![NPM version](https://img.shields.io/npm/v/@fastify/caching.svg?style=flat)](https://www.npmjs.com/package/@fastify/caching)
-[![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg?style=flat)](https://standardjs.com/)
+[![neostandard javascript style](https://img.shields.io/badge/code_style-neostandard-brightgreen?style=flat)](https://github.com/neostandard/neostandard)
 
 *@fastify/caching* is a plugin for the [Fastify](http://fastify.dev/) framework
 that provides server-side caching and mechanisms for manipulating HTTP cache headers according to
--- types/index.test-d.ts
+++ types/index.test-d.ts
@@ -1,69 +1,69 @@
-import Fastify, { FastifyReply } from 'fastify';
-import { expectAssignable, expectError, expectType } from 'tsd';
+import Fastify, { FastifyReply } from 'fastify'
+import { expectAssignable, expectError, expectType } from 'tsd'
 import fastifyCaching, {
   AbstractCacheCompliantObject,
   FastifyCachingPluginOptions,
-} from '..';
+} from '..'
 
-const fastify = Fastify({ logger: true });
+const fastify = Fastify({ logger: true })
 
 const cachingOptions: FastifyCachingPluginOptions = {
   privacy: fastifyCaching.privacy.PUBLIC,
   expiresIn: 300,
   cacheSegment: 'fastify-caching',
-};
-expectAssignable<FastifyCachingPluginOptions>(cachingOptions);
+}
+expectAssignable<FastifyCachingPluginOptions>(cachingOptions)
 
-fastify.register(fastifyCaching, cachingOptions);
+fastify.register(fastifyCaching, cachingOptions)
 
-expectType<AbstractCacheCompliantObject>(fastify.cache);
-expectType<AbstractCacheCompliantObject['get']>(fastify.cache.get);
-expectType<AbstractCacheCompliantObject['set']>(fastify.cache.set);
-expectType<string>(fastify.cacheSegment);
+expectType<AbstractCacheCompliantObject>(fastify.cache)
+expectType<AbstractCacheCompliantObject['get']>(fastify.cache.get)
+expectType<AbstractCacheCompliantObject['set']>(fastify.cache.set)
+expectType<string>(fastify.cacheSegment)
 // expectType<number>(fastify.etagMaxLife);
 
 fastify.get('/one', async (request, reply) => {
-  expectType<(tag?: string, timeToLive?: number) => FastifyReply>(reply.etag);
-  expectType<(date?: Date) => FastifyReply>(reply.expires);
+  expectType<(tag?: string, timeToLive?: number) => FastifyReply>(reply.etag)
+  expectType<(date?: Date) => FastifyReply>(reply.expires)
 
-  expectType<FastifyReply>(reply.etag('hello', 6000));
-  expectType<FastifyReply>(reply.expires(new Date(Date.now() + 6000)));
+  expectType<FastifyReply>(reply.etag('hello', 6000))
+  expectType<FastifyReply>(reply.expires(new Date(Date.now() + 6000)))
 
-  return { message: 'one' };
-});
+  return { message: 'one' }
+})
 
 fastify.get('/two', async (request, reply) => {
   expectType<FastifyReply>(
     reply.etag('hello', 6000).expires(new Date(Date.now() + 6000))
-  );
+  )
 
-  return { message: 'two' };
-});
+  return { message: 'two' }
+})
 
 // We register a new instance that should trigger a typescript error.
-const shouldErrorApp = Fastify({ logger: true });
+const shouldErrorApp = Fastify({ logger: true })
 
 const badCachingOptions = {
   privacy: fastifyCaching.privacy.PRIVATE,
   expiresIn: 'a string instead of a number of second',
   cacheSegment: 'fastify-caching',
-};
+}
 
-expectError(shouldErrorApp.register(fastifyCaching, badCachingOptions));
+expectError(shouldErrorApp.register(fastifyCaching, badCachingOptions))
 
 fastify.get('/three', async (request, reply) => {
   expectAssignable<Promise<unknown>>(
     fastify.cache.get('well-known')
-  );
+  )
   expectAssignable<Promise<{ item: string; stored: number; ttl: number; } | null>>(
     fastify.cache.get<string>('well-known')
-  );
+  )
   expectType<void>(
     fastify.cache.get<string>('well-known', (err, value) => {
-      expectType<unknown>(err);
-      expectAssignable<{ item: string; stored: number; ttl: number; } | null>(value);
+      expectType<unknown>(err)
+      expectAssignable<{ item: string; stored: number; ttl: number; } | null>(value)
     })
-  );
+  )
 
-  return { message: 'two' };
-});
+  return { message: 'two' }
+})
